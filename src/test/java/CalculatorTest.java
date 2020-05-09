a53fd1e5339e2afd81435fcdcd68343b484e8a07
vanya34 <gitvanya34@yandex.ru>
2020-05-10 00:14:14 +0400

    dev

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,8 +8,10 @@ class CalculatorTest {
         Calculator calculator=new Calculator();
         assertEquals(3 ,calculator.sum(1.0,2.0));
     }
+    @org.junit.jupiter.api.Test
     public void abs() throws Exception
     {
         assertEquals(5 ,Math.abs(-5));
     }
+
 }
